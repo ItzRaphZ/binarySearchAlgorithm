ac7ff11c3cae80805a6b3f302812e6b249485eb3
Rafael Dias <rafaelprazeresdias@gmail.com>
2020-04-23 18:15:52 +0100

    txt file added and python reads from txt file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Binary Search Algorithm")
+#Save the list in a local variable
+file = open("list.txt", "r")
+
+l = []
+
+tmp = 0
+for x in file:
+    l.append(file.readline())
+    l[tmp] = l[tmp][:-1]
+    tmp+=1
+
+file.close()
