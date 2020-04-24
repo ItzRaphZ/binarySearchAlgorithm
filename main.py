3ffabd01eb2b408083980a25d5bf4bf17a7a7e94
Rafael Dias <rafaelprazeresdias@gmail.com>
2020-04-24 03:57:22 +0100

    Started the algorithm

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 #Save the list in a local variable
 file = open("list.txt", "r")
 
@@ -5,8 +7,46 @@ l = []
 
 tmp = 0
 for x in file:
-    l.append(file.readline())
+    l.append(x)
     l[tmp] = l[tmp][:-1]
     tmp+=1
 
 file.close()
+
+#Creates the left, middle, right variables
+firstNum = l[0]
+
+if len(l)%2==0:
+    middle = len(l)/2
+    middle -= 1
+else:
+    middle = (len(l)-1)/2
+
+middle = math.floor(middle)
+middleNum = l[middle]
+
+lastNum = l[len(l)-1]
+
+#Create the algorithm
+Running = True
+
+while Running == True:
+    print("Answer true or false.")
+    option = input(f"Is the number higher than {middleNum}: ").lower()
+
+    if option == "true":
+        print("true")
+    elif option == "false":
+        print("false")
+    else:
+        print(f"\n\nERROR: Answer not \"true\" or \"false\". Please answer again.")
+
+    print("\n\n")
+
+    
+
+
+
+print(firstNum)
+print(middleNum)
+print(lastNum)
